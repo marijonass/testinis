505b9b433ca91e6f61d9529bfc38cb0556f9110a
marijonass <40793587+marijonass@users.noreply.github.com>
2018-07-03 17:05:01 +0300

    nauja versija

---
+++ saskes/js/main.js
@@ -0,0 +1,8 @@
+function addrow() {
+    
+    var black_kv = document.getElementsByClassName(" column_black ");
+    var white_kv = document.getElementsByClassName("colum_white");
+    var 
+    
+    
+}
